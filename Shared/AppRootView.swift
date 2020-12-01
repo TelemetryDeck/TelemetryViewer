6b0c93e29a873fb0e3ab8191d1c859a201a18164
Daniel Jilg <winsmith@winsmith.de>
2020-12-01 13:40:27 +0100

    Specify the time window for all visible insights at once in the UI

--- Shared/AppRootView.swift
+++ Shared/AppRootView.swift
@@ -26,6 +26,40 @@ struct AppRootView: View {
     @State private var sidebarShownValue: Bool = false
     @State private var sidebarSection: AppRootSidebarSection = .InsightEditor
 
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter
+    }()
+
+    var timeIntervalDescription: String {
+        let displayTimeWindowEnd = api.timeWindowEnd ?? Date()
+        let displayTimeWindowBegin = api.timeWindowBeginning ?? displayTimeWindowEnd.addingTimeInterval(-60 * 60 * 24 * 30)
+
+        if api.timeWindowEnd == nil {
+            if api.timeWindowBeginning == nil {
+                return "Showing Last 30 Days"
+            } else {
+                let components = Calendar.current.dateComponents([.day], from: displayTimeWindowBegin, to: displayTimeWindowEnd)
+                return "Showing Last \(components.day ?? 0) Days" // TODO
+            }
+        } else {
+            return "\(dateFormatter.string(from: displayTimeWindowBegin)) – \(dateFormatter.string(from: displayTimeWindowEnd))"
+        }
+    }
+
+    func reloadVisibleInsights() {
+        guard
+            let app = app,
+            let insightGroup = api.insightGroups[app]?.first(where: { $0.id == selectedInsightGroupID })
+        else { return }
+
+        for insight in insightGroup.insights {
+            api.getInsightData(for: insight, in: insightGroup, in: app)
+        }
+    }
+
     var body: some View {
         let selectedInsightID = Binding<UUID?>(get: {
             self.selectedInsightIDValue
@@ -99,6 +133,47 @@ struct AppRootView: View {
             }
         }
         .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Menu {
+                    Section {
+                        Button(action: {
+                            api.timeWindowBeginning = Date().addingTimeInterval(-60 * 60 * 24 * 365)
+                            api.timeWindowEnd = nil
+                            reloadVisibleInsights()
+                        }) {
+                            Label("Last Year", systemImage: "calendar")
+                        }
+
+                        Button(action: {
+                            api.timeWindowBeginning = nil
+                            api.timeWindowEnd = nil
+                            reloadVisibleInsights()
+                        }) {
+                            Label("Last Month", systemImage: "calendar")
+                        }
+
+                        Button(action: {
+                            api.timeWindowBeginning = Date().addingTimeInterval(-60 * 60 * 24 * 7)
+                            api.timeWindowEnd = nil
+                            reloadVisibleInsights()
+                        }) {
+                            Label("Last Week", systemImage: "calendar")
+                        }
+
+                        Button(action: {
+                            api.timeWindowBeginning = Date().addingTimeInterval(-60 * 60 * 24)
+                            api.timeWindowEnd = nil
+                            reloadVisibleInsights()
+                        }) {
+                            Label("Today", systemImage: "calendar")
+                        }
+                    }
+                }
+                label: {
+                    Text(timeIntervalDescription)
+                }
+            }
+
             ToolbarItem {
                 if let app = app, let insightGroup = api.insightGroups[app]?.first(where: { $0.id == selectedInsightGroupID }) {
                     Button(action: {
