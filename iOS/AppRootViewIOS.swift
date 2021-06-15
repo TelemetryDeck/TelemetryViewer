3c9766b6e164fbc72f570416e06a181e4d604056
Daniel Jilg <winsmith@winsmith.de>
2021-06-15 22:07:46 +0200

    Update iOS App Root View to allow reordering and deleting

--- iOS/AppRootViewIOS.swift
+++ iOS/AppRootViewIOS.swift
@@ -11,118 +11,176 @@ struct AppRootView: View {
     @EnvironmentObject var appService: AppService
     @EnvironmentObject var insightService: InsightService
     @EnvironmentObject var insightCalculationService: InsightCalculationService
+
     @State private var showDatePicker: Bool = false
     @State private var newInsightGroupID: UUID?
 
     let appID: UUID
+
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            List {
+                ForEach(insightService.insightGroups(for: appID) ?? []) { insightGroup in
+                    Section(header: Text(insightGroup.title)) {
+                        let sortedInsights = insightGroup.insights.sorted {
+                            $0.order ?? 0 < $1.order ?? 0
+                        }
+
+                        InsightList(appID: appID, insightGroupID: insightGroup.id, insights: sortedInsights)
+
+                        newInsightButton(insightGroup: insightGroup)
+                        editInsightGroupButton(insightGroup: insightGroup)
+                    }
+                }
+            }
+            .navigationBarTitle(appService.getSelectedApp()?.name ?? "–")
+            .navigationBarItems(trailing: EditButton())
+
+            if showDatePicker {
+                datePickerBackground()
+                datePicker()
+            }
+        }
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button(insightCalculationService.timeIntervalDescription) {
+                    withAnimation {
+                        self.showDatePicker.toggle()
+                    }
+                }
+
+                Spacer()
+
+                Button {
+                    insightService.create(insightGroupNamed: "New Insight Group", for: appID)
+                } label: {
+                    Text("New Insight Group") // , systemImage: "plus")
+                }
+            }
+        }
+
+    }
 
     func newInsight(_ definitionRequestBody: InsightDefinitionRequestBody, appID: UUID) {
         guard let groupID = definitionRequestBody.groupID else { return }
         insightService.create(insightWith: definitionRequestBody, in: groupID, for: appID)
     }
 
-    var body: some View {
-        ZStack(alignment: .bottom) {
-            List {
-                if insightService.insightGroups(for: appID) == nil {
-                    ProgressView()
-                } else if (insightService.insightGroups(for: appID) ?? []).isEmpty {
-                    EmptyAppView(appID: appID)
-                } else {
-                    ForEach(insightService.insightGroups(for: appID) ?? []) { insightGroup in
-                        Section(header: Text(insightGroup.title)) {
-                            let sortedInsights = insightGroup.insights.sorted {
-                                if $0.isExpanded != $1.isExpanded {
-                                    return $0.isExpanded && !$1.isExpanded
-                                } else {
-                                    return $0.order ?? 0 < $1.order ?? 0
-                                }
-                            }
-
-                            ForEach(sortedInsights) { insight in
-                                let destination = InsightEditor(appID: appID, insightGroupID: insightGroup.id, insightID: insight.id)
-                                NavigationLink(destination: destination) {
-                                    InsightView(topSelectedInsightID: .constant(nil), appID: appID, insightGroupID: insightGroup.id, insightID: insight.id)
-                                }
-                                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
-                                .buttonStyle(CardButtonStyle(isSelected: false))
-                            }
-
-                            NavigationLink(destination: InsightGroupEditor(appID: appID, insightGroup: insightGroup)) {
-                                Label("Edit \(insightGroup.title)", systemImage: "pencil")
-                            }
-
-                            Button {
-                                newInsightGroupID = insightGroup.id
-                            } label: {
-                                Label("New Insight in \(insightGroup.title)", systemImage: "plus")
-                            }
-                            .actionSheet(item: $newInsightGroupID) { newInsightGroupID in
-                                ActionSheet(
-                                    title: Text("New Insight"),
-                                    message: Text("Please select one of these template insights to get started"),
-                                    buttons: [
-                                        .cancel { self.newInsightGroupID = nil },
-                                        .default(Text("Generic Time Series Insight")) { newInsight(.newTimeSeriesInsight(groupID: newInsightGroupID), appID: appID) },
-                                        .default(Text("Generic Breakdown Insight")) { newInsight(.newBreakdownInsight(groupID: newInsightGroupID), appID: appID) },
-
-                                        .default(Text("Daily Active Users")) { newInsight(.newDailyUserCountInsight(groupID: newInsightGroupID), appID: appID) },
-                                        .default(Text("Weekly Active Users")) { newInsight(.newDailyUserCountInsight(groupID: newInsightGroupID), appID: appID) },
-                                        .default(Text("Monthly Active Users")) { newInsight(.newMonthlyUserCountInsight(groupID: newInsightGroupID), appID: appID) },
-                                        .default(Text("Daily Signals")) { newInsight(.newSignalInsight(groupID: newInsightGroupID), appID: appID) },
-
-                                        .default(Text("App Versions Breakdown")) { newInsight(.newBreakdownInsight(groupID: newInsightGroupID, title: "App Versions Breakdown", breakdownKey: "appVersion"), appID: appID) },
-                                        .default(Text("Build Number Breakdown")) { newInsight(.newBreakdownInsight(groupID: newInsightGroupID, title: "Build Number Breakdown", breakdownKey: "buildNumber"), appID: appID) },
-                                        .default(Text("Device Type Breakdown")) { newInsight(.newBreakdownInsight(groupID: newInsightGroupID, title: "Device Type Breakdown", breakdownKey: "modelName"), appID: appID) },
-                                        .default(Text("OS Breakdown")) { newInsight(.newBreakdownInsight(groupID: newInsightGroupID, title: "OS Breakdown", breakdownKey: "systemVersion"), appID: appID) }
-                                    ]
-                                )
-                            }
-                        }
-                    }
+    func datePickerBackground() -> some View {
+        Rectangle()
+            .fill(Color.grayColor.opacity(0.4))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea()
+            .onTapGesture {
+                withAnimation {
+                    showDatePicker.toggle()
                 }
             }
-            .navigationBarTitle(appService.getSelectedApp()?.name ?? "–")
-            .toolbar {
-                ToolbarItem {
-                    Button(insightCalculationService.timeIntervalDescription) {
-                        withAnimation {
-                            self.showDatePicker.toggle()
-                        }
-                    }
-                }
-                
-                ToolbarItemGroup(placement: .bottomBar) {
-                    Button {
-                        insightService.create(insightGroupNamed: "New Insight Group", for: appID)
-                    } label: {
-                        Text("New Insight Group") //, systemImage: "plus")
-                    }
+    }
 
-                }
-            }
-
-            if showDatePicker {
-                Rectangle()
-                    .fill(Color.grayColor.opacity(0.4))
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .ignoresSafeArea()
-                    .onTapGesture {
-                        withAnimation {
-                            showDatePicker.toggle()
-                        }
-                    }
-
-                CardView {
-                    InsightDataTimeIntervalPicker()
-                        .padding()
-                }
-                .frame(maxHeight: 200)
+    func datePicker() -> some View {
+        CardView {
+            InsightDataTimeIntervalPicker()
                 .padding()
-                .transition(.move(edge: .bottom))
-            }
         }
-        
+        .frame(maxHeight: 200)
+        .padding()
+        .transition(.move(edge: .bottom))
+    }
+
+    func editInsightGroupButton(insightGroup: DTO.InsightGroup) -> some View {
+        NavigationLink(destination: InsightGroupEditor(appID: appID, insightGroup: insightGroup)) {
+            Label("Edit \(insightGroup.title)", systemImage: "pencil")
+        }
+    }
+
+    func newInsightButton(insightGroup: DTO.InsightGroup) -> some View {
+        Button {
+            newInsightGroupID = insightGroup.id
+        } label: {
+            Label("New Insight in \(insightGroup.title)", systemImage: "plus")
+        }
+        .actionSheet(item: $newInsightGroupID) { newInsightGroupID in
+            ActionSheet(
+                title: Text("New Insight"),
+                message: Text("Please select one of these template insights to get started"),
+                buttons: [
+                    .cancel { self.newInsightGroupID = nil },
+                    .default(Text("Generic Time Series Insight")) { newInsight(.newTimeSeriesInsight(groupID: newInsightGroupID), appID: appID) },
+                    .default(Text("Generic Breakdown Insight")) { newInsight(.newBreakdownInsight(groupID: newInsightGroupID), appID: appID) },
+
+                    .default(Text("Daily Active Users")) { newInsight(.newDailyUserCountInsight(groupID: newInsightGroupID), appID: appID) },
+                    .default(Text("Weekly Active Users")) { newInsight(.newDailyUserCountInsight(groupID: newInsightGroupID), appID: appID) },
+                    .default(Text("Monthly Active Users")) { newInsight(.newMonthlyUserCountInsight(groupID: newInsightGroupID), appID: appID) },
+                    .default(Text("Daily Signals")) { newInsight(.newSignalInsight(groupID: newInsightGroupID), appID: appID) },
+
+                    .default(Text("App Versions Breakdown")) { newInsight(.newBreakdownInsight(groupID: newInsightGroupID, title: "App Versions Breakdown", breakdownKey: "appVersion"), appID: appID) },
+                    .default(Text("Build Number Breakdown")) { newInsight(.newBreakdownInsight(groupID: newInsightGroupID, title: "Build Number Breakdown", breakdownKey: "buildNumber"), appID: appID) },
+                    .default(Text("Device Type Breakdown")) { newInsight(.newBreakdownInsight(groupID: newInsightGroupID, title: "Device Type Breakdown", breakdownKey: "modelName"), appID: appID) },
+                    .default(Text("OS Breakdown")) { newInsight(.newBreakdownInsight(groupID: newInsightGroupID, title: "OS Breakdown", breakdownKey: "systemVersion"), appID: appID) }
+                ]
+            )
+        }
+    }
+}
+
+struct InsightList: View {
+    @Environment(\.editMode) var isEditMode
+    @EnvironmentObject var insightService: InsightService
+
+    let appID: UUID
+    let insightGroupID: UUID
+
+    @State var insights: [DTO.InsightDTO]
+
+    var body: some View {
+        if insightService.insightGroups(for: appID) == nil {
+            ProgressView()
+        } else if (insightService.insightGroups(for: appID) ?? []).isEmpty {
+            EmptyAppView(appID: appID)
+        }
+
+        ForEach(insights) { data in
+            InsightView(topSelectedInsightID: .constant(nil), appID: appID, insightGroupID: insightGroupID, insightID: data.id)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
+                .buttonStyle(CardButtonStyle(isSelected: false))
+        }
+        .onMove(perform: move)
+        .onDelete(perform: delete)
+    }
+
+    private func delete(at offsets: IndexSet) {
+        for offset in offsets {
+            let data = insights[offset]
+            print("\(data.title)")
+
+            insightService.delete(insightID: data.id, in: insightGroupID, in: appID)
+        }
+    }
+
+    private func move(source: IndexSet, destination: Int) {
+        insights.move(fromOffsets: source, toOffset: destination)
+
+        for (order, insight) in insights.enumerated() {
+            let requestBody = InsightDefinitionRequestBody(
+                order: Double(order),
+                title: insight.title,
+                subtitle: insight.subtitle,
+                signalType: insight.signalType,
+                uniqueUser: insight.uniqueUser,
+                filters: insight.filters,
+                rollingWindowSize: insight.rollingWindowSize,
+                breakdownKey: insight.breakdownKey,
+                groupBy: insight.groupBy,
+                displayMode: insight.displayMode,
+                groupID: insight.group.first?.value ?? UUID(),
+                id: insight.id,
+                isExpanded: insight.isExpanded,
+                shouldUseDruid: insight.shouldUseDruid
+            )
+
+            insightService.update(insightID: insight.id, in: insightGroupID, in: appID, with: requestBody)
+        }
     }
 }
 
