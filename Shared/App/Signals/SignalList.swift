6dbb20542d464a59cbf31dd095d9d9c908a01635
Daniel Jilg <winsmith@winsmith.de>
2021-09-24 15:00:55 +0200

    Re-add way to select and inspect recent signals on macOS

--- Shared/App/Signals/SignalList.swift
+++ Shared/App/Signals/SignalList.swift
@@ -12,39 +12,53 @@ struct SignalList: View {
     @EnvironmentObject var signalsService: SignalsService
 
     @State var filterText: String = ""
+    @State var selectedSignal: DTO.IdentifiableSignal?
 
     let appID: UUID
 
     var body: some View {
-        List {
-            #if os(iOS)
-            TextField("Search", text: $filterText)
-                .padding(7)
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-            #endif
+        HStack(spacing: 0) {
+            List(selection: $selectedSignal) {
+                TextField("Search", text: $filterText)
 
-            SignalListExplanationView()
+                SignalListExplanationView()
 
-            if signalsService.signals(for: appID).isEmpty && !signalsService.isLoading(appID: appID) {
-                Text("You haven't received any Signals yet. Once your app is sending out signals, you'll find here a list of the latest ones.\n\nHint: Usually, apps using the Telemetry Swift Client will only send out Signals if they are compiled in the Release build configuration. If your schema is in Debug mode, no signals will be sent.")
-                    .font(.footnote)
-                    .foregroundColor(.grayColor)
-            }
-
-            let signals = signalsService.signals(for: appID)
-                .filter {
-                    filterText.isEmpty ||
-                        $0.type.lowercased().contains(filterText.lowercased()) ||
-                        $0.clientUser.lowercased().contains(filterText.lowercased())
+                if signalsService.signals(for: appID).isEmpty && !signalsService.isLoading(appID: appID) {
+                    Text("You haven't received any Signals yet. Once your app is sending out signals, you'll find here a list of the latest ones.\n\nHint: Usually, apps using the Telemetry Swift Client will only send out Signals if they are compiled in the Release build configuration. If your schema is in Debug mode, no signals will be sent.")
+                        .font(.footnote)
+                        .foregroundColor(.grayColor)
                 }
 
-            ForEach(signals, id: \.id) { signal in
-                NavigationLink(
-                    destination: SignalView(signal: signal.signal),
-                    label: { SignalListCell(signal: signal.signal) }
-                )
+                let signals = signalsService.signals(for: appID)
+                    .filter {
+                        filterText.isEmpty ||
+                            $0.type.lowercased().contains(filterText.lowercased()) ||
+                            $0.clientUser.lowercased().contains(filterText.lowercased())
+                    }
+
+                ForEach(signals, id: \.id) { signal in
+                    #if os(macOS)
+                    SignalListCell(signal: signal.signal).tag(signal)
+                    #else
+                    NavigationLink(
+                        destination: SignalView(signal: signal.signal),
+                        label: { SignalListCell(signal: signal.signal) }
+                    )
+                    #endif
+                }
             }
+            
+            #if os(macOS)
+            Divider()
+            if let selectedSignal = selectedSignal {
+                SignalView(signal: selectedSignal.signal)
+            } else {
+                Text("Select a Signal")
+                    .foregroundColor(.grayColor)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+            }
+            #endif
         }
         .navigationTitle("Recent Signals")
         .onAppear {
