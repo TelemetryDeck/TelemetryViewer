7d9e05a6dc719cc5caa831d7ff84b3fba4619fc7
Daniel Jilg <winsmith@winsmith.de>
2021-09-28 16:22:15 +0200

    Add Color+Hex

---
+++ Shared/Helpers + Extensions/Color+Hex.swift
@@ -0,0 +1,38 @@
+// https://stackoverflow.com/a/67805329/54547
+
+import SwiftUI
+
+extension Color {
+    init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        var red: Double = 0.0
+        var green: Double = 0.0
+        var blue: Double = 0.0
+        var opacity: Double = 1.0
+
+        let length = hexSanitized.count
+
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+
+        if length == 6 {
+            red = Double((rgb & 0xFF0000) >> 16) / 255.0
+            green = Double((rgb & 0x00FF00) >> 8) / 255.0
+            blue = Double(rgb & 0x0000FF) / 255.0
+
+        } else if length == 8 {
+            red = Double((rgb & 0xFF000000) >> 24) / 255.0
+            green = Double((rgb & 0x00FF0000) >> 16) / 255.0
+            blue = Double((rgb & 0x0000FF00) >> 8) / 255.0
+            opacity = Double(rgb & 0x000000FF) / 255.0
+
+        } else {
+            return nil
+        }
+
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}
