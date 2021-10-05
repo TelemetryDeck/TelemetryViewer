35ef59f56e19abf76321b5256e85b828513faf73
fireantprincess <carly.weird@gmail.com>
2021-10-05 15:41:54 +0200

    Intent! at the Disco

---
+++ TelemetryDeckIntents/IntentHandler.swift
@@ -0,0 +1,19 @@
+//
+//  IntentHandler.swift
+//  TelemetryDeckIntents
+//
+//  Created by Charlotte Böhm on 05.10.21.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
