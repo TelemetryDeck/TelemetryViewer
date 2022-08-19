d63892f115e53605ceae8bb4217e068f551c2b0f
Daniel Jilg <daniel@telemetrydeck.com>
2022-08-19 13:19:19 +0200

    Add comment about @AppStorage

--- Shared/Navigational Structure/LeftSidebarView.swift
+++ Shared/Navigational Structure/LeftSidebarView.swift
@@ -21,8 +21,14 @@ struct LeftSidebarView: View {
         @EnvironmentObject var updateService: UpdateService
     #endif
 
-    @AppStorage("sidebarSelectionExpandedSections") var expandedSections: [DTOv2.App.ID: Bool]?
-    @AppStorage("sidebarSelection") var sidebarSelection: LeftSidebarView.Selection?
+    // The following two lines were created as "@AppStorage" properties
+    // however, in more recent Xcode versions they seem to not compile any more
+    // so we're moving them to a @State for now.
+    // The downside is that the State is no longer preserved between launches,
+    // even though this worked before.
+    // see https://github.com/TelemetryDeck/TelemetryViewer/issues/132
+    @State var expandedSections: [DTOv2.App.ID: Bool]?
+    @State var sidebarSelection: LeftSidebarView.Selection?
 
     enum Selection: Codable, Hashable {
         case getStarted
